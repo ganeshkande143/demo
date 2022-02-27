2477eda50bef73f1f5f24bd300dec8a68ec7ad82
ganeshkande143 <50288423+ganeshkande143@users.noreply.github.com>
2022-02-27 12:13:14 +0530

    Update A.java

--- SEP_BATCH_AUTOMATION/src/DEMO/A.java
+++ SEP_BATCH_AUTOMATION/src/DEMO/A.java
@@ -14,7 +14,7 @@ public class A
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Ganesh\\Downloads\\chromedriver_win32\\chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
 		driver.get("https://www.mercurytravels.co.in/");
-		
+		driver.get("Google");
 		
 		Thread.sleep(3000);
 		driver.close();
